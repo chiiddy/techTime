138a572f6f70256955f6b9f3b102b788cff17acf
chidicolly11@gmail.com <chidicolly11@gmail.com>
2023-05-10 18:59:06 +0100

    website

---
+++ src/components/Home.jsx
@@ -0,0 +1,462 @@
+import React from 'react'
+import './home.css'
+import Logo from "../images/logo-5866753e.svg"
+import Page from "../images/page.svg"
+import Arrow from "../images/arrow_outward-6a83a9d5.svg"
+import Mapped from "../images/mapped.svg"
+import Zoom from "../images/zoom-ba3b3c3d.svg"
+import Stripe from "../images/stripe-5009e4c8.svg"
+import Monday from "../images/monday-24c35f7f.svg"
+import Dropbox from "../images/dropbox-d7692600.svg"
+import Slack from "../images/slack-11024e07.svg"
+import Audio from "../images/audio-2382226c.svg"
+import Live from "../images/live-e3588df0.svg"
+import Recorded from "../images/recorded-59065990.svg"
+import Notes from "../images/notes-4afbedb5.svg"
+import Hero from "../images/hero-276445fd.svg"
+import Users2 from "../images/users2-956dd543.svg"
+import Heros3 from "../images/heros3-b686fe52.svg"
+import Mentors from "../images/mentors-5588f1c3.svg"
+import Meetings from "../images/meetings-153bfa1f.svg"
+import Oneonone from "../images/oneonone-67e0906c.svg"
+import Affordable from "../images/affordable-4c14efff.svg"
+import Teacher from "../images/teacher-401503ac.svg"
+import {BiTimeFive} from "react-icons/bi"
+import {MdOutlineMenuBook} from "react-icons/md"
+import Leonardo from "../images/leonardo-dd68c1fa.svg"
+import Audience from "../images/Audience-3d7c338b.svg"
+import Jeffery from "../images/jeffery-8baa97e7.svg"
+import Computer from "../images/computer-29ebb534.svg"
+import Claret from "../images/claret-c3274bf6.svg"
+import Coding from "../images/coding-3d3e19ed.svg"
+import Jessica from "../images/jessica-07ee7844.svg"
+import Conference from "../images/conference-7b454d53.svg"
+import Samuel from "../images/samuel-e5df0bde.svg"
+import Chart from "../images/chart-44517fe2.svg"
+import Adam from "../images/adam-3931a01f.svg"
+import Bella from '../images/bella-db0939fe.svg'
+import Bryce from '../images/Bryce-f8c43f71.svg'
+import Community from '../images/community-2dc5b38e.svg'
+import Add from '../images/add-d5d75fa0.svg'
+import Last from '../images/last-5866754q.svg'
+import Facebook from '../images/facebook-4e592cb2.svg'
+import Twitter from '../images/twitter-4de28e62.svg'
+import Youtube from '../images/youtube-069fe375.svg'
+import Instagram from '../images/instagram-2f9fd7a8.svg'
+import Discord from '../images/discord-dc6ef389.svg'
+export default function Home() {
+  return (
+    <div>
+      <div className='errt'>
+  <ul className="header">
+  <img className="tech"src={Logo} alt="" />
+    <h5><li>Home</li></h5>
+    <h5><li>About Us</li></h5>
+    <h5><li>Courses</li></h5>
+    <h5><li>Testimonial</li></h5>
+    <h5><li>Community</li></h5>
+    <li><button className="enrol">Enroll Now</button></li>
+  </ul>
+  </div>
+  <div className="second">
+    <div>
+        <h1>Grow your skills to 
+            <p>advance your career</p> 
+            path</h1>
+    <p>Build Your Future With Our Quality Education. The Best And Largest All-</p>
+            <p>In-One Online Tutoring Platform In The World</p>
+            <button className='started'>Get Started Now <img src={Arrow} alt="" /></button>
+            <button className='enrolled'>Enroll</button>
+            <p><img src={Mapped} alt="" /> 255k+
+Previews </p>
+</div>
+<div className='pages'>
+<img className="" src={Page} alt="" />
+</div>
+</div>
+<div>
+    <ul className="links">
+      <li><img src={Zoom} alt="" /></li>
+      <li><img src={Stripe} alt="" /></li>
+      <li> <img src={Monday} alt="" /></li>
+      <li><img src={Dropbox} alt="" /></li>
+      <li><img src={Slack} alt="" /></li>
+    </ul>
+  </div>
+       <div className="third-body">
+        <div>
+        <p><h1>High quality video, audio & </h1></p>
+           <p><h1>live classes</h1></p>
+           <p>High-Definition Video Is Video Of Higher Resolution And Quality Than Standard </p>
+           <p>Definition. While There’s No Standard Meaning For High Definition, Generally Any </p>
+            <p>Standard Video Image</p>
+            <button className='view'>View Courses</button> 
+        
+        <span>
+          <ul className="audio">
+        <li><h3> <img src={Audio} alt="" />Audio Classes</h3></li>
+        <li><h3><img src={Live} alt="" /> Live Classes </h3></li>
+        </ul>
+        </span>
+        
+        
+          <span>
+            <ul className="recorded">
+            <li><h3><img src={Recorded} alt="" /> Recorded Classes</h3></li>
+            <li><h3><img src={Notes} alt="" />50+ Notes</h3></li>
+            </ul>
+            </span>
+            </div>
+        <div>
+          <img className="baby"src={Hero} alt="" /> 
+          <img src={Users2} alt="" />
+        </div>
+        </div>
+        <section className="heros">
+          <div>
+            <p><h1>This Is Why We Are Best </h1></p>
+            <p><h1>From Others</h1></p>
+            <p>High-Definition Video Is Video Of Higher Resolution And Quality Than Standard </p>
+            <p>Definition. While There’s No Standard Meaning For High Definition, Generally Any </p>
+            <p>Standard Video Image</p>
+            <img src={Heros3} alt="" />
+          </div>
+          <div className='experience'>
+          <ul className="mentors">
+            <li><img src={Mentors} alt="" />
+           <p><h2>Experienced Mentors</h2></p>
+<p>High-Definition Video Is Video Of Higher Resolution</p> 
+<p>And Quality Than Standard Definition. High-</p>
+<p>Definition Video Is Video Of Higher </p>
+  <p>Resolution And Quality Than Standard Definition.</p></li>
+  <li><img src={Meetings} alt="" />
+  <p><h2>One-On-One Meetings</h2></p>
+<p>High-Defination Video Is Video Of Higher Resolution</p> 
+<p>And Quality Than Standard Definition. High-</p>
+<p>Defination Video Is Video Of Higher Resolution And </p>
+<p>Quality Than Standard Definition.</p></li>
+  </ul>
+  <ul className="oneonone">
+    <li><img src={Oneonone} alt="" />
+    <p><h2>One-On-One Meetings</h2></p>
+<p>High-Defination Video Is Video Of Higher Resolution </p>
+<p>And Quality Than Standard Definition. High-</p>
+<p>Defination Video Is Video Of Higher Resolution And </p>
+<p>Quality Than Standard Definition.</p></li>
+<li><img src={Affordable} alt="" />
+<p><h2>Affordables Prices</h2></p>
+<p>High-Defination Video Is Video Of Higher Resolution </p>
+<p>And Quality Than Standard Definition. High-</p>
+<p>Defination Video Is Video Of Higher Resolution And </p>
+<p>Quality Than Standard Definition.</p></li>
+  </ul>
+          </div>
+        </section>
+        <section className="browse">
+          <p><h1>Browse Our Popular Courses</h1></p>
+          <p>High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. While There’s </p>
+          <p>No Standard Meaning For High Definition, Generally Any Standard Video Image</p>
+        </section>
+        <div className="all">
+          <ul className="categories">
+          <li><p><h6>All Categories</h6></p></li>
+          <li><p><h6>Design</h6></p></li>
+          <li><p><h6>Development</h6></p></li>
+          <li><p><h6>Marketing</h6></p></li>
+          </ul>
+        </div>
+        <section className="whole">
+          <div>
+            <img src={Teacher} alt="" />
+            <div>
+              <ul>
+            <button className='design'>Design</button>
+            <p>Introduction To User Research In Ux </p>
+            <p>Design</p>
+            </ul>
+            </div>
+            <span>
+            <ul className="twenty-three">
+            <li><BiTimeFive/> 23hrs 50mins</li>
+            <li><MdOutlineMenuBook /> 15 Lessons</li>
+            </ul>
+            </span>
+            <span>
+            <ul className="leonardo">
+              <li><img src={Leonardo} alt="" /> Leonardo Victor</li>
+              <li>$15.00</li>
+            </ul>
+            </span>
+          </div>
+          <div>
+            <img src={Audience} alt="" />
+            <div>
+              <ul>
+            <button className='market'>Marketing</button>
+            <p>Introduction To New Marketing</p> 
+            <p>Audience</p>
+            </ul>
+            </div>
+            <div>
+              <ul className="twenty-two">
+                <li><BiTimeFive/> 22hrs 30mins</li>
+                <li><MdOutlineMenuBook /> 22 Lessons</li>
+              </ul>
+              <ul className="jeffery">
+                <li><img src={Jeffery} alt="" />Jeffery Williams</li>
+                <li>$32.00</li>
+                </ul>
+          </div>
+          </div>
+          <div>
+            <img src={Computer} alt="" />
+            <div>
+              <ul>
+                  <button className='develop'>Development</button>
+                  <p>Introduction To Html, Css & </p>
+                  <p>Bootstrap</p>
+              </ul>
+              <div>
+                <ul className="forty-five">
+                  <li><BiTimeFive />45hrs 50mins</li>
+                  <li><MdOutlineMenuBook />55 Lessons</li>
+                </ul>
+              </div>
+              <div>
+                <ul className="claret">
+                  <li><img src={Claret} alt="" />Claretta Mason</li>
+                  <li>$55.00</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="coding">
+        <div>
+          <div>
+          <img src={Coding} alt="" />
+          </div>
+          <div>
+            <ul>
+              <button className='ment'>Development</button>
+              <p>Introduction To Javascript, Git &</p> 
+              <p>GitHub</p>
+            </ul>
+            </div>
+            <div>
+              <ul className="thirty">
+                <li><BiTimeFive />30hrs 50mins</li>
+                <li><MdOutlineMenuBook />22 Lessons</li>
+              </ul>
+            </div>
+            <div>
+              <ul className="jessica">
+                <li><img src={Jessica} alt="" />Jessica Duke</li>
+                <li>$45.00</li>
+              </ul>
+            </div>
+          </div>
+          <span>
+            <div>
+              <img src={Conference} alt="" />
+            </div>
+            <div>
+              <ul>
+                <button className='mark'>Marketing</button>
+                <p>Introduction To Outroom Marketing</p>
+                <p> Analysis</p>w
+              </ul>
+            </div>
+            <div>
+              <ul className="thirty-three">
+                <li><BiTimeFive />33hrs 50mins</li>
+                <li><MdOutlineMenuBook />26 Lessons</li>
+              </ul>
+            </div>
+            <div>
+              <ul className="samuel">
+                <li><img src={Samuel} alt="" />Samuel Jacobs</li>
+                <li>$25.00</li>
+              </ul>
+            </div>
+          </span>
+          <span>
+            <div>
+              <img src={Chart} alt="" /> 
+            </div>
+            <div>
+              <ul>
+                <button className='intro'>Marketing</button>
+                <p>Introduction To Live Marketing </p>
+                <p>Analysis</p>
+              </ul>
+            </div>
+            <div>
+              <ul className="ten">
+                <li><BiTimeFive />10hrs 50mins</li>
+                <li><MdOutlineMenuBook />32 Lessons</li>
+              </ul>
+            </div>
+            <div>
+              <ul className="adam">
+                <li><img src={Adam} alt="" />Adam Smith</li>
+                <li>$25.00</li>
+              </ul>
+            </div>
+          </span>
+        </section>
+        <div className="explore">
+        <button className="courses"><h2>Explore All Courses</h2></button>
+        </div>
+        <div className="saying">
+          <h2><p>What Our Clients Are Saying</p></h2>
+          <p>High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. While There’s No </p>
+          <p>Standard Meaning For High Definition, Generally Any Standard Video Image</p>
+        </div>
+        <section className="writings">
+        <div className='bella'>
+          <p>High-Defination Video Is Video Of Higher Resolution And Quality Than </p>
+          <p>Standard Definition. High-Defination Video Is Video Of Higher Resolution</p> 
+          <p> And Quality Than Standard Definition. Hbuild Your Future With Our </p>
+          <p>Quality Education. The Best And Largest All-In-One Online Tutoring </p>
+          <p>Platform In The Worldd Definition. High-Defination Video Is Video Of</p>
+          <p> Higher Resolution And Quality Than Standard Definition. Build Your Future</p>
+          <p> With Our Quality Education. The Best And Largest All-In-One Online </p>
+          <p>Tutoring Platform In The World</p>
+          <div>
+          <img src={Bella} alt=""/> 
+          <h3>  <p>
+          Bella Moon</p></h3>
+          <p>Product Designer</p>
+          </div>
+        </div>
+
+        <div className='Bryce'>
+          <p>High-Defination Video Is Video Of Higher Resolution And Quality Than </p>
+          <p>Standard Definition. High-Defination Video Is Video Of Higher Resolution</p> 
+          <p> And Quality Than Standard Definition. Hbuild Your Future With Our </p>
+          <p>Quality Education. The Best And Largest All-In-One Online Tutoring </p>
+          <p>Platform In The Worldd Definition. High-Defination Video Is Video Of</p>
+          <p> Higher Resolution And Quality Than Standard Definition. Build Your Future</p>
+          <p> With Our Quality Education. The Best And Largest All-In-One Online </p>
+          <p>Tutoring Platform In The World</p>
+          <div>
+            <img src={Bryce} alt="" />
+            <h3><p>
+          Bryce Jason</p></h3>
+          <p>Product Designer</p>
+          </div>
+        </div>
+
+        <div className='samuel'>
+          <p>High-Defination Video Is Video Of Higher Resolution And Quality Than </p>
+          <p>Standard Definition. High-Defination Video Is Video Of Higher Resolution</p> 
+          <p> And Quality Than Standard Definition. Hbuild Your Future With Our </p>
+          <p>Quality Education. The Best And Largest All-In-One Online Tutoring </p>
+          <p>Platform In The Worldd Definition. High-Defination Video Is Video Of</p>
+          <p> Higher Resolution And Quality Than Standard Definition. Build Your Future</p>
+          <p> With Our Quality Education. The Best And Largest All-In-One Online </p>
+          <p>Tutoring Platform In The World</p>
+          <div className='fortune'>
+            <img src={Samuel} alt="" />
+          <h3><p>
+          Samuel Fortune</p></h3>
+          <p>Product Designer</p>
+          </div>
+        </div>
+        </section>
+        <div className='community'>
+          <p>Join Our Community</p>
+          <h1><p>Are You Ready To Connect With The</p></h1> 
+          <h1><p>Future Talent Of The Tech World</p></h1>
+          <p>Meet Up With Other Techstars And Grow Together</p>
+          <img src={Community} alt=""/>
+          <button className='join'><h4>Join Our Community</h4></button>
+        </div>
+        <div className='question'>
+          <h2><strong><p>Frequently Asked Questions</p></strong></h2>
+          <p>High-Defination Video Is Video Of Higher Resolution And Quality Than Standard Definition. While</p>
+          <p>There’s No Standard Meaning For High Definition, Generally Any Standard Video Image</p>
+        </div>
+        <div className='trial'>
+        <div className='free'>
+          <h2><p>Is There A Free Trial Available?</p></h2>
+          <img src={Add} alt="" />
+        </div>
+        <hr></hr>
+        <div className='plan'>
+        <h2><p>Can I Change My Plan Later?</p></h2>
+        <img src={Add} alt="" />
+        </div>
+        <hr></hr>
+        <div className='time'>
+          <h2><p>Are The Courses Lifetime?</p></h2>
+          <img src={Add} alt="" />
+        </div>
+        <hr></hr>
+        <span className='after'>
+          <h2><p>Do I Get Certified After Taking Courses?</p></h2>
+          <img src={Add} alt="" />
+        </span>
+        <hr></hr>
+        <div className='reach'>
+          <h2><p>How Do I Reach Out To Mentors?</p></h2>
+          <img src={Add} alt="" />
+        </div>
+        <hr></hr>
+        <span className='job'>
+          <h2><p>Do We Get Job Ready After Taking Courses?</p></h2>
+          <img src={Add} alt="" />
+        </span>
+        <hr></hr>
+        </div>
+        <div className='footer'>
+          <div>
+        <img src={Last} alt="" />
+        <p>Reach Out To Us On Any Of Our Social </p>
+        <p>Media Networks</p>
+        <div className='socials'> 
+          <img src={Facebook} alt="" />
+          <img src={Twitter} alt="" />
+          <img src={Youtube} alt="" />
+          <img src={Instagram} alt="" />
+          <img src={Discord} alt="" />
+        </div>
+        </div>
+        <span>
+          <h2><p>Useful Links</p></h2>
+          <div>
+            <ul className='useful'>
+              <li>Home</li>
+              <li>About Us</li>
+              <li>Our Courses</li>
+              <li>Testimonial</li>
+              <li>Our Community</li>
+            </ul>
+          </div>
+        </span>
+        <div>
+          <h2><p>Community</p></h2>
+          <div className='center'>
+            <ul className='help'>
+              <li>Help Centers</li>
+              <li>Partners</li>
+              <li>Suggestion</li>
+              <li>Blog</li>
+              <li>News Letter</li>
+            </ul>
+          </div>
+        </div>
+        <span>
+          <h2><p>Subscribe Us</p></h2>
+          <div>
+          <input className='nft' type="text" placeholder='nft124@gmail.com' />
+          <button className='button'>Send Message</button>
+          </div>
+        </span>
+        </div>
+        
+        
+    </div>
+  )
+}
